f30acc7b1167ee19a2b5b856fac4b414b4f57428
shulamit <shlamit931@gmail.com>
2023-05-01 19:32:08 +0300

    try

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
+//('@render-api/v1.0#dnrc1ulf088q9j');
+
+
+import express from 'express'
+import sdkapi from'api'
+const sdk=sdkapi('@render-api/v1.0#dnrc1ulf088q9j')
 const app = express()
+
 sdk.auth('rnd_44yRgsDvqy7TAOk8IGZBDxIEC9Ym');
 sdk.getServices({ limit: '20' })
     .then(({ data }) => console.log(data))
@@ -14,4 +19,4 @@ app.get('/', (req, res) => {
         .catch(err => console.error(err));
 
 })
-app.listen(3001,()=>{console.log("hi")})
+app.listen(3000,()=>{console.log("hi")})
