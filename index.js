af298070c7745a711aab8d8d85deb7be5f672289
shulamit <shlamit931@gmail.com>
2023-05-03 23:13:04 +0300

    require

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-//('@render-api/v1.0#dnrc1ulf088q9j');
+const express = require("express");
+const app = express();
+const sdk=require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 
-import express from 'express'
-import sdkapi from'api'
-const sdk=sdkapi('@render-api/v1.0#dnrc1ulf088q9j')
-const app = express()
 
 sdk.auth('rnd_44yRgsDvqy7TAOk8IGZBDxIEC9Ym');
 sdk.getServices({ limit: '20' })
