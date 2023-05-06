28d0224bb8a05c67dc9fb7854831777cf3df1cef
shulamit <shlamit931@gmail.com>
2023-05-06 22:07:18 +0300

    prefering to build

--- index.js
+++ index.js
@@ -4,17 +4,11 @@ const sdk=require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 
 
-sdk.auth('rnd_44yRgsDvqy7TAOk8IGZBDxIEC9Ym');
-sdk.getServices({ limit: '20' })
-    .then(({ data }) => console.log(data))
-    .catch(err => console.error(err));
-
-
 app.get('/', (req, res) => {
     sdk.auth('rnd_44yRgsDvqy7TAOk8IGZBDxIEC9Ym');
     sdk.getServices({ limit: '20' })
-        .then(({ data }) => {console.log(data);res.send(data)})
+        .then(({ data }) => {res.send(data)})
         .catch(err => console.error(err));
 
 })
-app.listen(3000,()=>{console.log("hi")})
+//app.listen(3000)
